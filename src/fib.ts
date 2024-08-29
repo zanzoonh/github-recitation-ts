d269974e6e148bbe5128a739b958371c7d7adfe2
Mohamed Elzeni <msmohame@andrew.cmu.edu>
2024-08-29 18:35:14 +0300

    Translate fib.ts to TypeScript

--- src/fib.ts
+++ src/fib.ts
@@ -1,5 +1,5 @@
 // util function that computes the fibonacci numbers
-module.exports = function fibonacci(n) {
+module.exports = function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
   } else if (n == 0) {
